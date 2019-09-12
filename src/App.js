b4372958998d960ca7aa37c03c5e10765f765259
root <miodrag.cupic89@gmail.com>
2019-09-12 14:25:43 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React after measdfadsf
         </a>
       </header>
     </div>
